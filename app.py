95592fe7577a2070d134e06dbf7a1a262f3315bd
root <root@DESKTOP-DPKUQCI>
2023-04-21 08:50:43 +0000

    implementation du back-end pour le calcule et l'affichage du nbr de files et repertoies de l'utilisateur conneté

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 import hashlib
 from flask import(
@@ -36,19 +37,39 @@ def login():
             return render_template('login.html',error_auth='username ou password est incorrecte')
     else:
         if('user_id' in session):
-            return render_template('app.html', len=10)
+            return render_template('app.html', num_files=len(get_user_files()), num_dirs=len(get_user_dirs()), dirlen=10)
         return redirect('/')
 
 @app.route('/dir/<path:dir_path>')
 def dir(dir_path):
     (dirs, files) = userDal.getUserData('/' + dir_path)
-    return render_template('app.html', dirs = dirs, files=files,dirlen=len(dirs),filelen=len(files))
+    return render_template('app.html', dirs = dirs, files=files,dirlen=len(dirs),filelen=len(files), num_files=len(get_user_files()) ,num_dirs=len(get_user_dirs()),total_size=get_user_total_size())
 
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     userDal.logged = False
     return redirect('/')
+
+@app.route('/get_user_files')
+def get_user_files():
+    user_path = os.path.join('/home', session.get('user_id', ''))
+    return [f for f in os.listdir(user_path) if os.path.isfile(os.path.join(user_path, f))]
+    
+@app.route('/get_user_dirs')
+def get_user_dirs():
+    user_path = os.path.join('/home', session.get('user_id', ''))
+    return [f for f in os.listdir(user_path) if os.path.isdir(os.path.join(user_path, f))]
+
+
+def get_user_total_size():
+    user_path = os.path.join('/home', session.get('user_id', ''))
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(user_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
 
 if(__name__ == "__main__"):
     userDal = UserDao()
--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,14 @@
 document.querySelector('.dir').addEventListener('click', function(e) {
     e.preventDefault()
 })
+
+function getNumberOfFiles() {
+    $.ajax({
+      type: 'GET',
+      url: '/files_count',
+      success: function (response) {
+        $('#files-link').text('Files (' + response.count + ')');
+      }
+    });
+  }
+  
